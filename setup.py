8d31f12b34318b55a73a3914d79b62c5cb323abd
dkumar <dkumar@lbl.gov>
2019-07-23 16:23:27 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'multitau',
+    version = '0.1.0',
+    description = 'Multi-tau autocorrelation for Synchroton data',
+    author = 'Dinesh Kumar',
+    author_email = 'dkumar@lbl.gov',
+    license = 'BSD',
+    keywords = 'synchrotron xpcs',
+    packages = [ 'multitau' ]
+)
