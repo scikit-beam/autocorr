27d79252fd88570606846bc12979893b3c1c5a38
dkumar <dkumar@lbl.gov>
2019-07-29 16:30:59 -0700

    using setup tools to build c++/cuda code

--- setup.py
+++ setup.py
@@ -1,4 +1,41 @@
-from setuptools import setup
+import os
+import subprocess
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.src_dir = os.path.abspath(sourcedir)
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError('cmake is not installed. Install cmake and try again')
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+        cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+        build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp) 
+       
 
 setup(
     name = 'multitau',
@@ -8,5 +45,8 @@ setup(
     author_email = 'dkumar@lbl.gov',
     license = 'BSD',
     keywords = 'synchrotron xpcs',
+    install_requires = [ 'pybind11' ],
+    ext_modules = [ CMakeExtension('multitau.cMultitau', 'src') ],
+    cmdlass = dict(build_ext = CMakeBuild),
     packages = [ 'multitau' ]
 )
