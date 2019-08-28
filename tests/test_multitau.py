64bd3dc6a063646f2e69ce5b1bf972ecc2bba2cf
Dan Allan <dallan@bnl.gov>
2019-08-28 14:27:45 -0400

    Refactor tests as pytest-compatible functions.

--- tests/test_multitau.py
+++ tests/test_multitau.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import multitau
 import time
 
@@ -8,25 +7,22 @@ np.random.seed(0)
 t = np.arange(N)
 A =  np.exp(-0.05 * t)[:,np.newaxis] + np.random.rand(N, 24) * 0.1
 
-plt.subplot(121)
-plt.semilogx(t, A.mean(axis=1))
-plt.title('Useless signal')
+def test_autocorrelate():
+    t0 = time.time()
+    g1, tau1 = multitau.autocorrelate(A.T, 16)
+    t1 = time.time()
+    print('pure python time = %f' % (t1-t0))
 
-t0 = time.time()
-g1, tau1 = multitau.autocorrelate(A.T, 16)
-t1 = time.time()
-g2, tau2 = multitau.autocorrelate_mt(A.T, 16)
-t2 = time.time()
-g3, tau3 = multitau.fftautocorr(A.T)
-t3 = time.time()
 
-print('pure python time = %f' % (t1-t0))
-print('accelrated version = %f' % (t2-t1))
-print('fft version = %f' % (t3-t2))
-plt.subplot(122)
-plt.semilogx(tau1, g1.mean(axis=0), 'o', label='py')
-plt.semilogx(tau2, g2.mean(axis=0), '.', label='c')
-plt.semilogx(tau3, g3.mean(axis=0), '.', label='fft')
-plt.title('Autocorrelations')
-plt.legend()
-plt.savefig('autocorr.png')
+def test_autocorrelate_mt():
+    t0 = time.time()
+    g1, tau1 = multitau.autocorrelate_mt(A.T, 16)
+    t1 = time.time()
+    print('accelrated version = %f' % (t1-t0))
+
+
+def test_fftautocorr():
+    t0 = time.time()
+    g3, tau3 = multitau.fftautocorr(A.T)
+    t1 = time.time()
+    print('fft version = %f' % (t1-t0))
