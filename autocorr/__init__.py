38ec5e7c033b955f34bdcc146b865ec47d193f8c
dkumar <dkumar@lbl.gov>
2019-08-28 15:46:17 -0700

    Removed README.md

--- autocorr/__init__.py
+++ autocorr/__init__.py
@@ -1,5 +1,6 @@
 
 import sys
+import warnings
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
@@ -11,6 +12,5 @@ try:
     from .cMultitau import multitau_mt
 except ImportError:
     def multitau_mt(signal, lags_per_level=16):
-        sys.stderr.write('multithreaded c-extension is missing.')
-        sys.stderr.write('falling back to python version')
+        warnings.warn('multithreaded c-extension is missing.', ImportWarning)
         return multitau(signal, lags_per_level)
