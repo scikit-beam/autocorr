da87f55afba62881fa3e37332d465c5a9bd05f9f
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-02-07 19:34:47 +0000

    Update docs after building Travis build 33 of scikit-beam/autocorr
    The docs were built from the branch 'master' against the commit
    cc939bbcfcdd781eebaf3e1a37fe125bb25987cb.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/scikit-beam/autocorr/jobs/647480655.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1.post2+gdd47b01',
+    VERSION: '0.0.1.post6+gcc939bb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
